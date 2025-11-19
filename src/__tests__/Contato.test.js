07d83fa79984353de588fdf91857357a645fd025
HenriqueRotsen <henriquerotsensf@gmail.com>
2025-11-19 08:44:50 -0300

    Arrumando erro

--- src/__tests__/Contato.test.js
+++ src/__tests__/Contato.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-import { Contato } from './pages/Contato';
+import { Contato } from '../pages/Contato';
 import emailjs from '@emailjs/browser';
 
 // 1. Mock do EmailJS (Para não enviar email real)
