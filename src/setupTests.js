af40d052322a35435e914e6de4e5adb806393b42
HenriqueRotsen <henriquerotsensf@gmail.com>
2025-11-19 09:07:28 -0300

    atualizando pacotes

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import pt from "../public/locales/pt/translation.json";
+import '@testing-library/jest-dom';
 
 i18n
   .use(initReactI18next)
