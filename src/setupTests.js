d5c82e58229d1098562902a580c33c8bc55df6ad
HenriqueRotsen <henriquerotsensf@gmail.com>
2026-06-09 18:07:59 -0300

    Ajuste email

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,6 +3,22 @@ import { initReactI18next } from "react-i18next";
 import pt from "../public/locales/pt/translation.json";
 import '@testing-library/jest-dom';
 
+class IntersectionObserverMock {
+  constructor(callback) {
+    this.callback = callback;
+  }
+
+  observe(element) {
+    this.callback([{ isIntersecting: true, target: element }]);
+  }
+
+  unobserve() {}
+
+  disconnect() {}
+}
+
+global.IntersectionObserver = IntersectionObserverMock;
+
 i18n
   .use(initReactI18next)
   .init({
